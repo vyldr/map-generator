83df040ed46437d4c8f1a760a8d7dcccead57c6d
wyld16steven <wyld16steven@gmail.com>
2020-12-04 22:21:16 -0600

    Added gui with save dialog

--- map-generator.py
+++ map-generator.py
@@ -1,135 +1,7 @@
-import time
-import os
-import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QDialog
+
+from MainWindow import Ui_MainWindow
 import mapgen
-
-
-def main():
-
-    map_generator = mapgen.Mapgen()
-    parameters = map_generator.parameters
-
-    # Apply any args from the command line
-    for i in range(1, len(sys.argv) - 1):
-        if sys.argv[i][0] == '-':
-            if isInt(sys.argv[i + 1]):
-                parameters[sys.argv[i][1:]] = int(sys.argv[i + 1])
-            elif isFloat(sys.argv[i + 1]):
-                parameters[sys.argv[i][1:]] = float(sys.argv[i + 1])
-            elif isBool(sys.argv[i + 1]):
-                parameters[sys.argv[i][1:]] = isTrue(sys.argv[i + 1])
-            else:
-                parameters[sys.argv[i][1:]] = sys.argv[i + 1]
-
-    # Start the timer
-    start = time.process_time()
-
-    # Keep trying until we get one or give up after 100 times
-    success = map_generator.mapgen()
-    if success:
-        if parameters["save"]:
-            saveFile(parameters["name"], map_generator.MMtext, parameters["overwrite"])
-
-    else:
-        print("Unable to create a map with those parameters")
-
-   # Stop timing
-    finish = time.process_time()
-
-    # Optionally show the stats
-    print()
-    if parameters["stats"]:
-        print('Parameters:')
-        for key in parameters:
-            print('    ', key, ': ', parameters[key], sep='')
-
-        print('\nResults:')
-        print('    Size: ', parameters["length"], 'x', parameters["width"])
-        print('    Tiles:', parameters["length"] * parameters["width"])
-        print('    Time: ', finish - start, 'seconds')
-        if finish - start == 0:
-            print('Finished with infinite speed!')
-        else:
-            print('    Speed:', (parameters["length"] * parameters["width"]) / (finish - start), 'tiles per second')
-        print()
-
-    if parameters["stats"]:
-        input()
-
-
-# Display on the command line
-def display(array1):
-    colors = {
-        '1':  '\033[48;2;24;0;59m',  # Ground
-        '101':  '\033[48;2;24;0;59m',  # Ground
-        '26':  '\033[48;2;166;72;233m',  # Dirt
-        '30':  '\033[48;2;139;43;199m',  # Loose Rock
-        '34':  '\033[48;2;108;10;163m',  # Hard Rock
-        '38':  '\033[48;2;59;0;108m',  # Solid Rock
-        5:  '\033[48;2;61;38;20m',  # Unused
-        '11':  '\033[48;2;6;45;182m',  # Water
-        '111':  '\033[48;2;6;45;182m',  # Water
-        '6':  '\033[48;2;239;79;16m',  # Lava
-        '106':  '\033[48;2;239;79;16m',  # Lava
-        '63':  '\033[48;2;24;0;59m',  # Landslide rubble
-        '163':  '\033[48;2;24;0;59m',  # Landslide rubble
-        '12':  '\033[48;2;150;150;0m',  # Slimy Slug hole
-        '112':  '\033[48;2;150;150;0m',  # Slimy Slug hole
-        '42': '\033[48;2;185;255;25m',  # Energy Crystal Seam
-        '46': '\033[48;2;146;62;20m',  # Ore Seam
-        '50': '\033[48;2;250;255;14m',  # Recharge Seam
-        '14': '\033[48;2;190;190;190m',  # Building power path
-        '114': '\033[48;2;190;190;190m',  # Building power path
-    }
-    for i in range(len(array1)):
-        for j in range(len(array1[0])):
-            # Different color for each type
-            print(
-                colors[array1[i][j]],  # Background
-                '\033[38;2;0;255;16m',  # Text
-                '  ',
-                # '\033[38;2;255;255;255m',  # White text
-                # ' ',
-                end='', sep='')
-        print('\033[0m')  # Reset the background and new line
-
-
-# Display a 2D int array visually with scaled values
-def displayArr(array, stats=False):
-    # Find the limits
-    min = array[0][0]
-    max = array[0][0]
-    for i in range(len(array)):
-        for j in range(len(array[0])):
-            if array[i][j] < min:
-                min = array[i][j]
-            if array[i][j] > max:
-                max = array[i][j]
-
-    difference = max - min
-
-    # Print the array
-    for i in range(len(array)):
-        for j in range(len(array[0])):
-            value = int((array[i][j] - min) /
-                        (difference if difference else 1) * 255)
-            print('\033[48;2;', 0, ';', value, ';',
-                  0, 'm', '  ', sep='', end='')
-        print('\033[0m')  # Reset the background and new line
-
-    # Optionally display the unique values and their differences
-    if stats:
-        set = []
-        differences = []
-        for i in range(len(array)):
-            for j in range(len(array[0])):
-                if array[i][j] not in set:
-                    set.append(array[i][j])
-        set.sort()
-        print(set)
-        for i in range(len(set) - 1):
-            differences.append(set[i + 1] - set[i])
-        print(differences)
 
 
 # Display the map as a PNG
@@ -198,58 +70,41 @@ def displayPNG(wallArray, crystalArray, oreArray, landslideList, flowList):
     # img.save("output.png")
 
 
-# Check if a string is a bool
-def isBool(s):
-    if s == "True" or s == "true" or s == "False" or s == "false":
-        return True
-    return False
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.newMapButton.clicked.connect(self.generate_map)
+        self.saveButton.clicked.connect(self.saveFile)
+        self.map_generator = mapgen.Mapgen()
 
+    def generate_map(self):
+        self.map_generator.mapgen()
+        print(self.map_generator.MMtext)
 
-# Check if a string is a float
-def isFloat(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
+    # Save the output to a file
+    def saveFile(self):
 
+        # Set up the save dialog
+        save_dialog = QFileDialog()
+        save_dialog.setAcceptMode(QFileDialog.AcceptSave)
+        save_dialog.setWindowTitle('Save Map')
+        save_dialog.setNameFilter('Manic Miners level files (*.dat)')
+        save_dialog.setDefaultSuffix('dat')
+        if save_dialog.exec():
 
-# Check if a string is an int
-def isInt(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
-
-
-# Check if a string is true
-def isTrue(s):
-    if s == "True" or s == "true":
-        return True
-    return False
-
-
-# Save the output to a file
-def saveFile(filename, content, overwrite):
-    # Remove spaces from the filename
-    filename = filename.replace(' ', '')
-
-    # Make sure the filename is unique
-    counter = 0  # Increment if the file exists
-    if overwrite == False:
-        while os.path.isfile(filename + (str(counter) if counter else '') + '.dat'):
-            counter += 1
-    filename += (str(counter) if counter else '') + '.dat'
-
-    # Save the file
-    f = open(filename, 'w')
-    f.write(content)
-    f.close()
-    print("Saved to:", filename)
-    print('   ', os.path.join(os.getcwd(), filename))
-    print()
+            # Save the file
+            filename = save_dialog.selectedFiles()[0]
+            output_file = open(filename, 'w')
+            output_file.write(self.map_generator.MMtext)
+            output_file.close()
+            print("Saved to:", filename)
+            print()
 
 
 # Do the thing
-main()
+if __name__ == '__main__':
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec()
